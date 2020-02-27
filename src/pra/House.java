c4e202047823c720e8d41320668cc1431dffc4f7
tong.lu <tong.lu@chinacreator.com>
2020-02-27 16:27:37 +0800

    住址获取

--- src/pra/House.java
+++ src/pra/House.java
@@ -3,4 +3,13 @@ package pra;
 public class House {
 
 	private String Address;
+
+	public String getAddress() {
+		return Address;
+	}
+
+	public void setAddress(String address) {
+		Address = address;
+	}
+	
 }
