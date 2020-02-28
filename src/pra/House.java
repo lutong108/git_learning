bf2867396dcce584e2445495359e5644d1537b6e
tong.lu <tong.lu@chinacreator.com>
2020-02-28 11:49:31 +0800

    temp格式调整

--- src/pra/House.java
+++ src/pra/House.java
@@ -1,24 +1,18 @@
 package pra;
 
 public class House {
-
 	private String name;
-	private String belong;
-
 	public String getName() {
 		return name;
-	}
-
+	}	
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	private String belong;
 	public String getBelong() {
 		return belong;
 	}
-
 	public void setBelong(String belong) {
 		this.belong = belong;
 	}
-	
 }
