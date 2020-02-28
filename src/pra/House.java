70b8c18bd3f3f33b06792c132b0345f01d79d3bf
tong.lu <tong.lu@chinacreator.com>
2020-02-28 11:48:16 +0800

    master格式调整

--- src/pra/House.java
+++ src/pra/House.java
@@ -1,27 +1,23 @@
 package pra;
 
 public class House {
-
-	private String Address;
-
 	private String name;
-	
-	private Integer time;
-	
-	public String getAddress() {
-		return Address;
-	}
-
-	public void setAddress(String address) {
-		Address = address;
-	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
+	private String Address;
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	private Integer time;
+	
+
+
 	
 }
